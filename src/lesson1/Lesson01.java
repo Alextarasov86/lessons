99b2baaae731bdba0ea7d4eabca08a49338b8481
Alex <alex.t.86@mail.ru>
2024-02-05 20:18:59 +0300

    Создан файл lessons.properties, добавлены изменения в фаил Lesson01

--- src/lesson1/Lesson01.java
+++ src/lesson1/Lesson01.java
@@ -3,5 +3,6 @@ package lesson1;
 public class Lesson01 {
     public static void main(String[] args) {
         System.out.println("Lesson 01");
+        System.out.println("Hello world");
     }
 }
